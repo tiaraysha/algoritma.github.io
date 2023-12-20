e48aea75d5db5dd6b953619fc88a37dbe3735ad5
tiaraysha <sititiarayshaputriazahara18@gmail.com>
2023-12-20 10:22:37 +0700

    algoritma

---
+++ index.js
@@ -0,0 +1,68 @@
+// MENCAARI KELILING PERSEGI PANJANG
+// Memasukkan nilai panjang, lebar
+// Menghitung rumus: 2*P + 2*L
+// Menampilkan hasil
+
+// Memasukkan nilai panjang, lebar
+
+let panjang = 150;
+let lebar = 40;
+
+// Menghitung rumus
+let hasil = 2 * panjang + 2 * lebar;
+
+// Menampilkan
+console.log("Keliling persegi panjang adalah:" + hasil)
+
+// 2. Luas Lingkaran
+
+// Tentukan phi dan r
+let phi = 3.14;
+let r = 10;
+
+let hasilL = phi * r * r;
+
+console.log ("Hasil nya adalah:" + hasilL)
+
+// 3. Cari sisi persegi
+
+let Lp = 64;
+let sisi = Math.sqrt(Lp)
+
+let Hasil = sisi * 4;
+
+console.log ("Hasilnya adalah:" + Hasil)
+
+// 4. Algoritma membuat KTP
+// input
+ let umur = 18;
+
+// proses
+if (umur >= 17) {
+    result  = "Membuat KTP"
+}
+else {
+    result = "Belum bisa membuat KTP"
+}
+
+// output
+console.log (result)
+
+// 5. 
+let PABP = 86;
+let MATH = 82;
+let DPK = 77;
+
+let median = (PABP + MATH + DPK) / 3;
+
+if (median >= 80 && median <=100) {
+    grade = "A";
+}
+
+else if (median >=75 && median <=100) {
+    grade = "B";
+}
+
+console.log (grade)
+
+// Menentukan Bilangan Ganjil Genap
